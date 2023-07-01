1c3e172ac640c4aa55d1f0b104c3875706e10708
rawisha <rawvsmaw22@gmail.com>
2023-07-01 21:21:42 +0200

    fixes

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const express = require('express')
-const app = express()
-const axios = require('axios')
-const cors = require('cors')
-require('dotenv').config({path: __dirname + '/.env'})
-app.use(cors({
-    origin: '*',
-}))
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config({ path: __dirname + "/.env" });
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get(':endpoint([\\/\\w\\?\\=]*)', function(req,res){
-    
-    let endpoint = process.env.API_URL + req.params.endpoint.replace('/', '')
+app.get(":endpoint([\\/\\w\\?\\=]*)", function (req, res) {
+  let endpoint = process.env.API_URL + req.params.endpoint.replace("/", "");
 
-    axios.get(endpoint).then(response => {
-        res.json(response.data)
-       
-    }).catch(error => {
-        res.json(error)
+  axios
+    .get(endpoint)
+    .then((response) => {
+      if (response.data) {
+        res.json(response.data);
+      } else {
+        res.status(204).json({ error: "haay keeer.. bro bixwlenarawa" });
+      }
     })
-   
-})
+    .catch((error) => {
+      res.json(error);
+    });
+});
 
-
-
-const port = process.env.PORT || 3001 
-app.listen(port)
+const port = process.env.PORT || 3001;
+app.listen(port);
